dfcfe7785787d5e074e081fddadeb70cf0135c3e
Jean-Paul Bassil <Jean-Paul Bassil>
2024-06-17 02:15:08 +0300

    chore: Enable CORS in main.ts for API accessibility

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,19 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
+  app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle('Inspect App API')
     .setDescription('This is the API documentation for the Inspect Mobile App.')
     .setVersion('1.0')
     .build();
-    
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  Logger.log(`---------------------------------------`, 'Bootstrap');
   Logger.log(`---------------------------------------`, 'Bootstrap');
   Logger.log(`                                       `, 'Bootstrap');
   Logger.log(`Server running on http://localhost:3200`, 'Bootstrap');
@@ -22,8 +25,8 @@ async function bootstrap() {
   Logger.log(`API running on http://localhost:3200/api`, 'Bootstrap');
   Logger.log(`                                       `, 'Bootstrap');
   Logger.log(`---------------------------------------`, 'Bootstrap');
+  Logger.log(`---------------------------------------`, 'Bootstrap');
 
-  
   await app.listen(3200);
 }
 bootstrap();
