7764083ac19931d8544cabc38195f9aeb04a2f25
Jean-Paul Bassil <Jean-Paul Bassil>
2024-06-17 02:04:44 +0300

    feat: implemented the guard and the jwt service

--- src/auth/jwt-auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth/jwt-auth.guard.ts
@@ -1,5 +1,49 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+// guards/jwt-auth.guard.ts
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AuthService } from '../auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from 'interfaces/jwt.payload';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard implements CanActivate {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException('Token not found');
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync<JwtPayload>(token);
+      const user = await this.prisma.user.findUnique({
+        where: { id: payload.id },
+      });
+      if (!user) {
+        throw new UnauthorizedException('User not found');
+      }
+      request.user = user;
+      return true;
+    } catch (error) {
+      throw new UnauthorizedException('Invalid token');
+    }
+  }
+
+  private extractTokenFromHeader(request: any): string | null {
+    const authorization = request.headers.authorization;
+    if (authorization && authorization.startsWith('Bearer ')) {
+      return authorization.split(' ')[1];
+    }
+    return null;
+  }
+}
