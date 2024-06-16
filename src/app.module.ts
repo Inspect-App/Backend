b17cb381374c0e3e4092245b5af8502757e256a6
Jean-Paul Bassil <Jean-Paul Bassil>
2024-06-17 02:04:59 +0300

    chore: Update AppModule and main.ts for centralized configuration access

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma.service';
+import { CustomConfigService } from './config/config.service';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, CustomConfigService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,7 +14,16 @@ async function bootstrap() {
     
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  Logger.log(`---------------------------------------`, 'Bootstrap');
+  Logger.log(`                                       `, 'Bootstrap');
+  Logger.log(`Server running on http://localhost:3200`, 'Bootstrap');
+  Logger.log(`                                       `, 'Bootstrap');
+  Logger.log(`API running on http://localhost:3200/api`, 'Bootstrap');
+  Logger.log(`                                       `, 'Bootstrap');
+  Logger.log(`---------------------------------------`, 'Bootstrap');
+
   
-  await app.listen(3000);
+  await app.listen(3200);
 }
 bootstrap();
