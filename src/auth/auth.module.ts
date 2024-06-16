61b45e0820f5f1669c61c6971e855ba12b640c90
Jean-Paul Bassil <Jean-Paul Bassil>
2024-06-17 00:56:40 +0300

    feat: Add PrismaService to AppModule and AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt/jwt.service';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
-  providers: [AuthService, JwtStrategy],
-  controllers: [AuthController]
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '15m' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy, PrismaService],
 })
 export class AuthModule {}
