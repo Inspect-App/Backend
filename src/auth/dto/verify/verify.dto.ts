a2e235d3ee6c18a8fcd51f747e008eac01d55bbe
Jean-Paul Bassil <Jean-Paul Bassil>
2024-06-17 03:42:19 +0300

    feat: Add email validation to RegisterDto and VerifyDto

--- src/auth/dto/verify/verify.dto.ts
+++ src/auth/dto/verify/verify.dto.ts
@@ -1,4 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString } from 'class-validator';
+
 export class VerifyDto {
+  @ApiProperty({ example: 'user@example.com', type: String })
+  @IsEmail()
   email: string;
+
+  @ApiProperty({ example: '123456', type: String })
+  @IsString()
   verificationCode: string;
 }
