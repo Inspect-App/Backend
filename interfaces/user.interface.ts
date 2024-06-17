09c52ffccf1d88bb8b663c4c5d0b9c4cf1b14812
Jean-Paul Bassil <Jean-Paul Bassil>
2024-06-17 03:11:51 +0300

    feat: Add firstName, lastName, and verificationCode fields to User interface

--- interfaces/user.interface.ts
+++ interfaces/user.interface.ts
@@ -2,6 +2,10 @@ export interface User {
   id: number;
   email: string;
   password: string;
+  firstName: string;
+  lastName: string;
+  verificationCode?: string;
+  isVerified: boolean;
   refreshToken?: string;
   createdAt: Date;
   updatedAt: Date;
