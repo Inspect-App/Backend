79b1e3dd89c36f0962a162dcd0103e2bbc07a96f
Georges5091 <gnehme31@gmail.com>
2024-11-01 09:11:27 +0200

    feat(file): setup update, delete and delete all files

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Put } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { Status } from '@prisma/client';
+import { UpdateStatusDto } from './dto/update-status.dto';
 
 @Controller('files')
 export class FilesController {
@@ -9,5 +10,20 @@ export class FilesController {
     @Get('/:status')
     async getByStatus(@Param('status') status: Status) {
         return this.filesService.getByStatus(status);
+    }
+
+    @Put('/:id')
+    async updateFileStatus(@Param('id', ParseIntPipe) id: number, @Body() statusDto: UpdateStatusDto) {
+        return this.filesService.updateFileStatus(id, statusDto);
+    }
+
+    @Delete('/:id')
+    async deleteFile(@Param('id', ParseIntPipe) id: number) {
+        return this.filesService.deleteFile(id);
+    }
+
+    @Delete()
+    async deleteAll() {
+        return this.filesService.deleteAll();
     }
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { FileDto } from './dto/file.dto';
 import { Status } from '@prisma/client';
+import { UpdateStatusDto } from './dto/update-status.dto';
 
 @Injectable()
 export class FilesService {
@@ -9,5 +10,24 @@ export class FilesService {
 
     async getByStatus(status: Status): Promise<FileDto[]> {
         return this.prismaService.file.findMany({where: {status: status}});
+    }
+
+    async updateFileStatus(id: number, statusDto: UpdateStatusDto) {
+        const { status } = statusDto;
+
+        return this.prismaService.file.update(
+            {
+                where: { id },
+                data: { status }
+            }
+        )
+    }
+    
+    async deleteFile(id: number) {
+        return this.prismaService.file.delete({where: {id}});
+    }
+
+    async deleteAll() {
+        return this.prismaService.file.deleteMany({where: {status: Status.deleted}})
     }
 }
